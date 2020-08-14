598dabeaf8a4f1a2bdca70c09259e5ccc74b79c2
Farhan1706 <36563944+Farhan1706@users.noreply.github.com>
2020-08-14 18:28:01 +0700

    DB-Rangakaian-Script Nodemcu

---
+++ #PENTING/testing.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 14, 2020 at 01:27 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `testing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `serialnumber` double NOT NULL,
+  `gender` enum('Male','Female') NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `fingerprint_id` int(11) NOT NULL,
+  `fingerprint_select` tinyint(1) NOT NULL DEFAULT '0',
+  `user_date` date NOT NULL,
+  `time_in` time NOT NULL,
+  `del_fingerid` tinyint(1) NOT NULL DEFAULT '0',
+  `add_fingerid` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `serialnumber`, `gender`, `email`, `fingerprint_id`, `fingerprint_select`, `user_date`, `time_in`, `del_fingerid`, `add_fingerid`) VALUES
+(5, 'fnaufal', 17113302, 'Male', 'vnalvaro24@gmail.com', 0, 0, '2020-08-14', '14:00:00', 0, 0),
+(6, 'Naufal', 11111, 'Female', 'nori876bg@gmail.com', 4, 1, '2020-08-14', '21:53:00', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_logs`
+--
+
+CREATE TABLE `users_logs` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `serialnumber` double NOT NULL,
+  `fingerprint_id` int(5) NOT NULL,
+  `checkindate` date NOT NULL,
+  `timein` time NOT NULL,
+  `timeout` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users_logs`
+--
+ALTER TABLE `users_logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users_logs`
+--
+ALTER TABLE `users_logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
